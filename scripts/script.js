36d89e739ce131d4a341e01eea5a83a016ddd797
nielsbakker04 <144062046+nielsbakker04@users.noreply.github.com>
2023-09-21 14:24:22 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+hamburgerMenu = document.querySelector(".menuIngeklapt");
+hamburgerKnop = document.querySelector(".hamburgerKnop")
+
+hamburgerKnop.onclick = menuUitklappen;
+
+function menuUitklappen(){
+    hamburgerMenu.classList.toggle("menuUitgeklapt");
+}
